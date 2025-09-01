f37f868b18a27ec6921c6728389588097a31b3ea
eeexde <edrianthelazy@gmail.com>
2025-09-01 10:16:47 +0800

    Updated the email

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -3,7 +3,7 @@ import Section from '../components/Section'
 const Contact: React.FC = () => (
   <Section id="contact" headline="Get in touch" description="Have a question or want to work together?">
     <div className="contact">
-      <a className="btn" href="mailto:you@example.com">Email me</a>
+      <a className="btn" href="mailto:pasaylo.ed03@gmail.com">Email me</a>
       <div className="socials">
         <a href="https://github.com/eeexde" target="_blank" rel="noreferrer">GitHub</a>
       </div>
